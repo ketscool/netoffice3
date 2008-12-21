6b2f82a0c99f28670acb63446e42a60dcdfc2a8f
Indemnity83 <Indemnity83@28b99530-ceb4-11dd-8772-d1b041bf0e37>
2008-12-21 10:27:05 +0000

    current database schema

---
+++ schema.sql
@@ -0,0 +1,547 @@
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: netoffice3
+-- ------------------------------------------------------
+-- Server version	5.0.67
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `clients` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `title` varchar(50) NOT NULL,
+  `acronym` varchar(10) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,'Airtop Technology Group, Inc','ATG','590 Menlo Drive, Suite 5 Rocklin, CA 95765','2008-12-20 23:58:05','2008-12-20 23:58:05');
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clients_notes`
+--
+
+DROP TABLE IF EXISTS `clients_notes`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `clients_notes` (
+  `id` int(11) NOT NULL auto_increment,
+  `client_id` int(11) NOT NULL,
+  `note_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=FIXED;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `clients_notes`
+--
+
+LOCK TABLES `clients_notes` WRITE;
+/*!40000 ALTER TABLE `clients_notes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clients_notes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entries`
+--
+
+DROP TABLE IF EXISTS `entries`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `entries` (
+  `id` int(10) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `access` int(1) NOT NULL default '0',
+  `modified` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `user_id` int(10) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `revision` int(5) NOT NULL,
+  `accessed` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `title` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `entries`
+--
+
+LOCK TABLES `entries` WRITE;
+/*!40000 ALTER TABLE `entries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entries_projects`
+--
+
+DROP TABLE IF EXISTS `entries_projects`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `entries_projects` (
+  `id` int(11) NOT NULL auto_increment,
+  `project_id` int(11) NOT NULL,
+  `entry_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `entries_projects`
+--
+
+LOCK TABLES `entries_projects` WRITE;
+/*!40000 ALTER TABLE `entries_projects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entries_projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entry_revisions`
+--
+
+DROP TABLE IF EXISTS `entry_revisions`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `entry_revisions` (
+  `id` int(10) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `access` int(1) NOT NULL default '0',
+  `modified` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `user_id` int(10) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `revision` int(5) NOT NULL,
+  `accessed` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `entry_revisions`
+--
+
+LOCK TABLES `entry_revisions` WRITE;
+/*!40000 ALTER TABLE `entry_revisions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entry_revisions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hours`
+--
+
+DROP TABLE IF EXISTS `hours`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `hours` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `time` float(5,1) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `hours`
+--
+
+LOCK TABLES `hours` WRITE;
+/*!40000 ALTER TABLE `hours` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hours` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notes`
+--
+
+DROP TABLE IF EXISTS `notes`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `body` text NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `notes`
+--
+
+LOCK TABLES `notes` WRITE;
+/*!40000 ALTER TABLE `notes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notes_projects`
+--
+
+DROP TABLE IF EXISTS `notes_projects`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `notes_projects` (
+  `id` int(11) NOT NULL auto_increment,
+  `project_id` int(11) NOT NULL,
+  `note_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=FIXED;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `notes_projects`
+--
+
+LOCK TABLES `notes_projects` WRITE;
+/*!40000 ALTER TABLE `notes_projects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notes_projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notes_tasks`
+--
+
+DROP TABLE IF EXISTS `notes_tasks`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `notes_tasks` (
+  `id` int(11) NOT NULL auto_increment,
+  `task_id` int(11) NOT NULL,
+  `note_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=FIXED;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `notes_tasks`
+--
+
+LOCK TABLES `notes_tasks` WRITE;
+/*!40000 ALTER TABLE `notes_tasks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notes_tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notes_users`
+--
+
+DROP TABLE IF EXISTS `notes_users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `notes_users` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `note_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=FIXED;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `notes_users`
+--
+
+LOCK TABLES `notes_users` WRITE;
+/*!40000 ALTER TABLE `notes_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notes_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `projects` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `client_id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (1,1,'NetOffice3','Development work on NetOffice3',1,'2008-12-21 00:09:10','2008-12-21 01:01:01');
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects_uploads`
+--
+
+DROP TABLE IF EXISTS `projects_uploads`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `projects_uploads` (
+  `id` int(11) NOT NULL auto_increment,
+  `project_id` int(11) NOT NULL,
+  `upload_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `projects_uploads`
+--
+
+LOCK TABLES `projects_uploads` WRITE;
+/*!40000 ALTER TABLE `projects_uploads` DISABLE KEYS */;
+/*!40000 ALTER TABLE `projects_uploads` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects_users`
+--
+
+DROP TABLE IF EXISTS `projects_users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `projects_users` (
+  `id` int(11) NOT NULL auto_increment,
+  `project_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `projects_users`
+--
+
+LOCK TABLES `projects_users` WRITE;
+/*!40000 ALTER TABLE `projects_users` DISABLE KEYS */;
+INSERT INTO `projects_users` VALUES (4,1,1);
+/*!40000 ALTER TABLE `projects_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `services`
+--
+
+DROP TABLE IF EXISTS `services`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `services` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `value` float(10,2) NOT NULL,
+  `unit` varchar(10) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `services`
+--
+
+LOCK TABLES `services` WRITE;
+/*!40000 ALTER TABLE `services` DISABLE KEYS */;
+INSERT INTO `services` VALUES (1,'Overhead','',0.00,'$');
+/*!40000 ALTER TABLE `services` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `settings`
+--
+
+DROP TABLE IF EXISTS `settings`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `settings` (
+  `id` int(3) NOT NULL auto_increment,
+  `name` varchar(50) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `default` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `settings`
+--
+
+LOCK TABLES `settings` WRITE;
+/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `tasks` (
+  `id` int(11) unsigned NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `assigned_user_id` int(11) NOT NULL,
+  `progress` decimal(3,2) NOT NULL,
+  `start` datetime NOT NULL,
+  `due` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `upload_revisions`
+--
+
+DROP TABLE IF EXISTS `upload_revisions`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `upload_revisions` (
+  `id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `access` int(1) NOT NULL,
+  `modified` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `user_id` int(10) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `revision` int(5) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `upload_revisions`
+--
+
+LOCK TABLES `upload_revisions` WRITE;
+/*!40000 ALTER TABLE `upload_revisions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `upload_revisions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uploads`
+--
+
+DROP TABLE IF EXISTS `uploads`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `uploads` (
+  `id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `access` int(1) NOT NULL,
+  `modified` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `user_id` int(10) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `revision` int(5) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `filename` (`filename`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `uploads`
+--
+
+LOCK TABLES `uploads` WRITE;
+/*!40000 ALTER TABLE `uploads` DISABLE KEYS */;
+/*!40000 ALTER TABLE `uploads` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `access` int(1) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `language` varchar(12) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `login` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `login` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Kyle Klaus','kklaus','sha*twt',0,'kklaus@airtop.net','','',1,'2008-12-20 23:58:20','2008-12-20 23:58:20','0000-00-00 00:00:00'),(2,'Jeramie Wheeler','jwheeler','atg!2005',0,'jwheeler@airtop.net','','',1,'2008-12-21 01:00:36','2008-12-21 01:00:36','0000-00-00 00:00:00');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2008-12-21 10:26:36
