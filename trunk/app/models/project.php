6c08af70fbc52bc891c6619706bfd6085724b133
Indemnity83 <Indemnity83@28b99530-ceb4-11dd-8772-d1b041bf0e37>
2008-12-21 21:10:38 +0000

    Built basic layout

--- trunk/app/models/project.php
+++ trunk/app/models/project.php
@@ -3,7 +3,12 @@ class Project extends AppModel
 {
    var $name = 'Project';
    var $belongsTo = array ('Client', 'Service');
-   var $hasMany = array ('Task', 'Hour');
+   var $hasMany = array (
+		'Task' => array(  
+			'className'  => 'Task',  
+			'dependent'  => true), 
+		'Hour'
+	);
    var $hasAndBelongsToMany = array ('User', 'Note', 'Upload', 'Entry');
 }
 ?>
